1d2a29812bcb3b23a328f0222b85c071f38fe283
Joe Numainville <josephbnumainville@gmail.com>
2017-06-20 10:29:03 -0500

    connection architecture created and tested

--- inv.php
+++ inv.php
@@ -3,6 +3,29 @@
   <head>
   </head>
   <body>
-    <?php  ?>
+    <?php
+      /*$servername, $username, $password, and $port are used to connect to the
+      mySQL server. change as necessary for your installation. 3306 is the
+      default port used by WAMP. */
+      $servername = "localhost";
+      $username = "user";
+      $password = "password";
+      $port = 3306;
+
+      //Creates a new connection to the server utilizing variables above.
+      $conn = new mysqli($servername, $username, $password, "", port);
+
+      /* Below checks to ensure a connection to the server is made. If failed,
+      the error message is given and the user is instructed to retry. */
+      if ($conn->connect_error) {
+        die("Connection failed for the following reason: " .
+        $conn->connect_error . ". Please try again");
+      }
+      else {
+        echo "Connection established.";
+      }
+
+      $conn->close();
+    ?>
   </body>
 </html>
