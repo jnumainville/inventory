450d937f262f005eed3a48990fb849199de38bc0
Joe Numainville <josephbnumainville@gmail.com>
2017-06-21 10:22:04 -0500

    readded table.php after accidental deletion

---
+++ table.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+      /*$servername, $username, $password, and $port are used to connect to the
+      mySQL server. change as necessary for your installation. 3306 is the
+      default port used by WAMP. */
+      $servername = "localhost";
+      $username = "user";
+      $password = "password";
+      $dbname = "inv";
+      $port = 3306;
+
+      //Creates a new connection to the server utilizing variables above.
+      $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+      /* Below checks to ensure a connection to the server is made. If failed,
+      the error message is given. */
+      if ($conn->connect_error) {
+        die("Connection failed for the following reason: " .
+        $conn->connect_error);
+      }
+      else {
+        echo "Connection established.";
+      }
+
+
+      // Below is the sql to create the table for the task management
+      // system with the specified columns
+      $createTab = "CREATE TABLE Tasks (
+        id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT(65535),
+        assigned TIMESTAMP,
+        due DATE,
+        dueTime TIME
+      )";
+
+      // Below ensures that the creation of the table is successful. If not, it
+      // instructs the user to try again.
+      if ($conn->query($createTab) === TRUE) {
+        echo "Table created successfully";
+      } else {
+        echo "Error creating table: " . $conn->error;
+      }
+      $conn->close();
+    ?>
+  </body>
+</html>
