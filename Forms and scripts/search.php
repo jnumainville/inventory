a5bbf447e1d602b64b940fdc749f19bdbcb333e1
Joe Numainville <josephbnumainville@gmail.com>
2017-06-26 11:38:08 -0500

    some work done on search, saving work

--- Forms and scripts/search.php
+++ Forms and scripts/search.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="../style.css">
   </head>
   <body>
     <h2>Results:</h2>
@@ -19,31 +20,48 @@
 
     //below is the prepared statement for searching
     $stmt = $conn->prepare("SELECT * FROM Tasks WHERE id = ? OR title = ?
-    OR description = ? OR due = ? OR dueTime = ?");
-    $stmt->bind_param("issss", $id, $title, $description, $due, $dueTime);
+    OR description = ? OR due = ? OR dueTime = ? OR completed = ?
+    ORDER BY id DESC");
+    $stmt->bind_param("isssss", $id, $title, $description, $due, $dueTime,
+    $completed);
 
     //variable assignments for search from viewform.html
+    //$id = $_POST['id'];
     $id = $_POST['id'];
-    $title = $_POST['title']
+    $title = $_POST['title'];
     $description = $_POST['description'];
     $due = $_POST['due'];
     $dueTime = $_POST['dueTime'];
+    $completed = $_POST['completed'];
 
     //executes the search given parameters and statement above
     $stmt->execute();
-    $stmt->bind_result($id, $title, $description, $due, $dueTime);
+    $result = $stmt->get_result();
 
     // output data of each row
-    if ($query->num_rows > 0) {
-      while($row = $query->fetch_assoc()) {
-        echo "Number: " . $row['id'] .  " " . "|" . " Title: " . $row['title']
-        . " " . "|" . " Description: " . $row['description'] . " " . "|"
-        . " Time due: " . $row['due'] . " " . $row['dueTime'] . " " . "|"
-        . " Completed: " . $row['completed'] . "<br>";
+    if ($result->num_rows > 0) {
+      //headers of table in format: Number Title Description Time Due Completed
+      /*echo ("<table><tr><th>Number</th>"
+      . "<th>Title</th><th>Description</th><th>Time Due</th>
+      . <th>Completed</th></tr>");
+      while($row = $result->fetch_assoc()) {
+        $id[] = $row['id'];
+        $title[] = $row['title'];
+        $description[] = $row['description'];
+        $due[] = $row['due'];
+        $dueTime[] = $row['dueTime'];
+        $completed[] = $row['completed'];
       }
+      for (int i = 0; i < count($id); i++) {
+        echo ("<tr><td>" . $id[i] . "</td><td>" . $title[i]
+        . "</td><td>" . $description[i] . "</td><td>" . $due[i]
+        . " " . $dueTime[i] . "</td><td>" . $completed[i]
+        . "</td></tr>");
+      }
+      echo "</table>"; */
     }
     else {
-      echo "Your search returned no results.";
+      echo "<p>Your search returned no results.</p>";
     }
 
     $stmt->close();
